8f92b7f89bc2ca4832bd19b4a228ff055eaa33b2
Ricky Lin <rlin61@stuy.edu>
2024-09-27 10:12:04 -0400

    Added test cases for returnCopy and aryToString

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,8 +1,13 @@
 public class MyArray{
   public static void main(Strings[] args){
-
+  // test for aryToString
+  int[] nums = {4, 5, 6};
+  System.out.println(aryToString(nums));
+  //test for returnCopy
+  System.out.println(aryToString(returnCopy(ary)).equals(ary));
+  System.out.println(aryToString(returnCopy(ary)) == ary);
   }
-  public static String aryToSTring(int[] nums){
+  public static String aryToString(int[] nums){
     String s = "[";
     for (int i = 0; i < nums.length; i++){
       s += nums[i];
@@ -13,4 +18,6 @@ public class MyArray{
     }
     return s;
   }
+  public static int[] returnCopy(int[]ary)
+
 }
