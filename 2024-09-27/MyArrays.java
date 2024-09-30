178c2080bfbf540b453dd875177ee998c82a9123
Ricky Lin <rlin61@stuy.edu>
2024-09-29 23:24:47 -0400

    Added more test cases for each method

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -3,9 +3,28 @@ public class MyArrays{
   // test for aryToString
   int[] nums = new int[] {4, 5, 6};
   System.out.println(aryToString(nums).equals("[4, 5, 6]"));
+  int[] nums1 = new int[] {1, 3, 4, 5};
+  System.out.println(aryToString(nums1).equals("[1, 3, 4, 5]"));
+  int[] nums2 = new int[] {};
+  System.out.println(aryToString(nums2).equals("[]"));
   //test for returnCopy
+int[] list1 = {4, 5, 6, 1};
+  System.out.println(aryToString(returnCopy(list1)).equals(aryToString(list1)));
+  System.out.println(returnCopy(list1) != list1);
+  int[] list2 = {99, 10, 14, 13, 141, 41};
+    System.out.println(aryToString(returnCopy(list2)).equals(aryToString(list2)));
+    System.out.println(returnCopy(list2) != list2);
 
-  System.out.println(aryToString(returnCopy(ary)).equals(ary));
+  //test for concatArray
+  int[] ary1 = {1, 2};
+  int[] ary2 = {3, 4};
+  int[] concatenated = concatArray(ary1, ary2);
+  System.out.println(aryToString(concatenated).equals("[1, 2, 3, 4]"));
+  int[] ary3 = {1, 24, 12};
+  int[] ary4 = {94, 104, 101, 11};
+  int[] concatenated1 = concatArray(ary3, ary4);
+  System.out.println(aryToString(concatenated1).equals("[1, 24, 12, 94, 104, 101, 11]"));
+
   }
   public static String aryToString(int[] nums){
     String s = "[";
@@ -14,8 +33,9 @@ public class MyArrays{
       if (i < nums.length -1){
         s += ", ";
       }
-      s += "]";
+
     }
+    s += "]";
     return s;
   }
   public static int[] returnCopy(int[] ary){
