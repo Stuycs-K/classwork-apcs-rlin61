5017c66721c845ccf0e42f1bc59aee52f8614360
Ricky Lin <rlin61@stuy.edu>
2024-09-29 22:20:43 -0400

    added code for concatArray

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -6,7 +6,6 @@ public class MyArrays{
   //test for returnCopy
 
   System.out.println(aryToString(returnCopy(ary)).equals(ary));
-  System.out.println(aryToString(returnCopy(ary)) == ary);
   }
   public static String aryToString(int[] nums){
     String s = "[";
@@ -23,6 +22,16 @@ public class MyArrays{
   int[] copy = new int[ary.length];
   for(int i = 0; i < ary.length; i++){
     copy[i] = ary[i];
+  }
+  return copy;
+}
+public static int[] concatArray(int[]ary1,int[]ary2){
+  int[] copy = new int[ary1.length + ary2.length];
+  for (int i = 0; i < ary1.length; i++){
+    copy[i] = ary1[i];
+  }
+  for (int i = 0; i < ary2.length; i++){
+    copy[i + (ary1.length)] = ary2[i];
   }
   return copy;
 }
