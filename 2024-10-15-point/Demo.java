01e1acadf57be09286f72adcaa4935da8ae6d986
Ricky Lin <rlin61@stuy.edu>
2024-10-15 22:49:30 -0400

    Fixed distance() method

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -5,7 +5,7 @@ public class Demo{
   to the others.
   */
   public static double distance(Point a, Point b){
-    return Math.sqrt(((a.getX() - b.getX()) * (a.getX() - b.getX())) - ((a.getY() - b.getY()) * (a.getY() - b.getY())));
+    return Math.sqrt(Math.pow((a.getX() - b.getX()), 2) + Math.pow((a.getY() - b.getY()), 2 ));
   }
 
   public static void main(String[]args){
@@ -14,6 +14,7 @@ public class Demo{
     Point p3 = new Point(3,4);
 
     System.out.println( p3);
+    System.out.println( distance(p3,p1));
     System.out.println( distance(p1,p2));
     System.out.println( Point.distance(p1,p2));
     System.out.println( p1.distanceTo(p2));
