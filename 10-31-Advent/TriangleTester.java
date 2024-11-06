41a8e7be4a7aaa60fb9c59ff879b6b1863433155
Ricky Lin <rlin61@stuy.edu>
2024-11-05 23:16:53 -0500

    Fixed part a and added code for part b

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class TriangleTester{
    public static int countTrianglesA(String filename){
      int count = 0;
@@ -8,8 +9,38 @@ public class TriangleTester{
        File file = new File(filename);
        Scanner input = new Scanner(file);
        while (input.hasNextLine()){
-         input.nextLine();
          if (testTriangle(input.nextInt(), input.nextInt(), input.nextInt())){
+           count += 1;
+         }
+       }
+       input.close();
+     } catch (FileNotFoundException ex) {
+       System.out.println("File not found");
+       System.exit(1);
+     }
+     return count;
+   }
+   public static int countTrianglesB(String filename){
+     ArrayList<int[]>arrOfRow = new ArrayList<int[]>();
+     int count = 0;
+     try {
+       File file = new File(filename);
+       Scanner input = new Scanner(file);
+       while(input.hasNextLine()){
+         int[] row = new int[3];
+         for (int i = 0; i < 3; i++){
+             row[i] = input.nextInt();
+         }
+         arrOfRow.add(row);
+       }
+       for (int i = 0; i < arrOfRow.size(); i+=3){
+         if (testTriangle(arrOfRow.get(i)[0], arrOfRow.get(i + 1)[0], arrOfRow.get(i + 2)[0])){
+           count += 1;
+         }
+         if (testTriangle(arrOfRow.get(i)[1], arrOfRow.get(i + 1)[1], arrOfRow.get(i + 2)[1])){
+           count += 1;
+         }
+         if (testTriangle(arrOfRow.get(i)[2], arrOfRow.get(i + 1)[2], arrOfRow.get(i + 2)[2])){
            count += 1;
          }
        }
@@ -25,5 +56,6 @@ public class TriangleTester{
    }
    public static void main(String[] args){
    System.out.println(countTrianglesA("inputTri.txt"));
+   System.out.println(countTrianglesB("inputTri.txt"));
  }
-}
+ }
