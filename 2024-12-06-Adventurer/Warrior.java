f49e353c1c935518f1e13d79b24adda851b4c7f2
Ricky Lin <rlin61@stuy.edu>
2024-12-08 23:50:49 -0500

    Added code for all the accessor methods and attack(other) support(other)

--- 2024-12-06-Adventurer/Warrior.java
+++ 2024-12-06-Adventurer/Warrior.java
@@ -1,4 +1,6 @@
-public class Warrior implements Adventurer{
+public class Warrior extends Adventurer{
+  private String specialName = "Rage";
+  private int special, specialMax;
   public Warrior(String name){
     super(name);
   }
@@ -6,7 +8,29 @@ public class Warrior implements Adventurer{
     super(name, hp);
   }
   public String getSpecialName(){
-    return "rage";
+    return specialName;
   }
-  public int getSpecial();
+  public int getSpecial(){
+    return special;
+  }
+  public void setSpecial(int n){
+    special = n;
+  }
+  public int getSpecialMax(){
+    return specialMax;
+  }
+  public String attack(Adventurer other){
+    other.applyDamage(10);
+    System.out.println(this.getName() + " attacked " + other.getName());
+  }
+  public String support(Adventurer other){
+    other.setHP(other.getHP() + 10);
+    System.out.println(this.getName() + " healed " + other.getName());
+
+  }
+  public String support();
+  public String specialAttack(Adventurer other);
+
+
+
 }
