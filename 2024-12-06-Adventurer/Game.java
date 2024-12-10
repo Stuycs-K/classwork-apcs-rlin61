e2623d8ac831d1ef1f1c66241a48c92387923845
Ricky Lin <rlin61@stuy.edu>
2024-12-10 00:59:47 -0500

    Added code for the while loop for players to choose action, do the action, and printed it out

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -8,8 +8,29 @@ public class Game{
     while(true){
       System.out.println(player.getName() + ", " + player.getHP() + "/" + player.getMaxHP() + " HP, " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName());
       System.out.println(enemy.getName() + ", " + enemy.getHP() + "/" + enemy.getMaxHP() + " HP, " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
-      System.out.println("Type: (a)ttack / (sp)ecial / (s1) support / (s2) support / quit")
-  }
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+      String input = userInput.nextLine();
+      while(!input.equals("a") || !input.equals("attack")
+        || !input.equals("sp")|| !input.equals("special")
+        || !input.equals("su") || !input.equals("support")
+        || !input.equals("quit")){
+        System.out.println("Invalid response. Please input a valid response:");
+        String input = userInput.nextLine();
+      }
+      if (input.equals("quit")){
+        System.out.println("User quit");
+        return;
+      }
+      else if (input.equals("a") ||input.equals("attack") ){
+        System.out.println(player.attack(enemy));
+      }
+      else if (input.equals("sp") ||input.equals("special") ){
+        System.out.println(player.specialAttack(enemy));
+      }
+      else if (input.equals("su") ||input.equals("support") ){
+        System.out.println(player.support());
+      }
+      
   }
 
 }
