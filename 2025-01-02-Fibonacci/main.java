397fc5777f5cd23a903001a5fe4ce50121e7b083
Ricky Lin <rlin61@stuy.edu>
2025-01-03 09:15:29 -0600

    Started working on makeWords(). Starting on the loop

--- 2025-01-02-Fibonacci/main.java
+++ 2025-01-02-Fibonacci/main.java
@@ -1,6 +1,7 @@
 public class main{
     public static void main(String[] args){
-      fib(46);
+      //fib(46);
+      makeWords(2, "", "abc");
     }
     public static int fib(int n){
       int sum = 0;
@@ -12,5 +13,10 @@ public class main{
       }
       return sum;
     }
+    public static void makeWords(int remainingLetters, String result, String alphabet){
+      // use loop through alphabet
+    for (int i = 0; i < alphabet.length(); i++){
+      alphabet.get(i)
+    }
+}
   }
-  
